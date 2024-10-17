ae8357996158408bbbfc77d5398b0523f615c428
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-10-17 17:52:23 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,11 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, astuple
+
+import requests
+from bs4 import BeautifulSoup, Tag
+
+
+FIELDS = ["text", "author", "tags"]
 
 
 @dataclass
@@ -8,8 +15,37 @@ class Quote:
     tags: list[str]
 
 
+def parse_single_quote(quote_soup: Tag) -> Quote:
+    return Quote(
+        text=quote_soup.select_one(".text").text.replace("“", "").replace("”", ""),
+        author=quote_soup.select_one(".author").text,
+        tags=[tag.text for tag in quote_soup.select("a.tag")]
+    )
+
+
+def get_quotes() -> list[Quote]:
+    n = 1
+    objects = []
+    while True:
+        page = requests.get(f"https://quotes.toscrape.com/page/{n}/").content
+        soup = BeautifulSoup(page, "html.parser")
+
+        quotes = soup.select(".quote")
+        res = [parse_single_quote(quote) for quote in quotes]
+
+        objects.extend(res)
+        n += 1
+        if not res:
+            break
+    return objects
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_quotes()
+    with open(output_csv_path, "w",  encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(FIELDS)
+        writer.writerows([astuple(quote_obj) for quote_obj in quotes])
 
 
 if __name__ == "__main__":
